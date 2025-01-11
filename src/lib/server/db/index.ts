85a0be1432613fd0c919860340fadd3795cb574a
Seba9989 <sgrzybbowski@gmail.com>
2025-01-11 14:40:39 +0100

    Full Fix

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,11 +5,9 @@ import * as schema from './schema';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
-if (!env.DATABASE_SYNC_URL) throw new Error('DATABASE_SYNC_URL is not set');
 
 const client = createClient({
-	url: env.DATABASE_SYNC_URL,
-	syncUrl: env.DATABASE_SYNC_URL,
+	url: env.DATABASE_URL,
 	authToken: env.DATABASE_AUTH_TOKEN
 });
 
