2de3b7976782ab65b1cd2fa15c6b767b70835a73
Seba9989 <sgrzybbowski@gmail.com>
2025-02-05 21:21:31 +0100

    quick-fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
 import '$lib/server/discord/index';
+import '$lib/index';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
